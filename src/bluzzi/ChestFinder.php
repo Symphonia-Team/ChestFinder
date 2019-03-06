2e4c317ade29084cb2165c91d1725807931e017b
Bluzzi <bluzzigame@gmail.com>
2019-03-06 23:12:29 +0100

    Create ChestFinder.php

---
+++ src/bluzzi/ChestFinder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace bluzzi;
+
+use pocketmine\scheduler\Task;
+
+use pocketmine\Player;
+
+use pocketmine\item\Item;;
+
+use pocketmine\tile\Chest;
+
+use pocketmine\utils\Config;
+
+class ChestFinder extends Task {
+
+    private $player;
+    private $event;
+    private $plugin;
+
+    public function __construct(Player $player, $plugin, $event){
+        $this->player = $player;
+        $this->plugin = $plugin;
+        $this->event = $event;
+    }
+
+    public function onRun(int $tick){
+        if(!($this->player->isOnline())){
+            unset($this->event->using[$this->player->getName()]);
+            $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+            return;
+        }
+
+        $itemHeld = $this->player->getInventory()->getItemInHand();
+        $item = Item::fromString($this->plugin->config->get("id"));
+
+        if(!($itemHeld->equals($item, true, false))){
+            unset($this->event->using[$this->player->getName()]);
+            $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+            return;
+        }
+
+        $chestCount = 0;
+        $theChest;
+
+        foreach($this->player->getLevel()->getTiles() as $tile){
+            if($tile instanceof Chest){
+                if($this->player->distance($tile) <= $this->plugin->config->get("radius")){
+                    if(empty($theChest)){
+                        $theChest = $tile;
+                    } else {
+                        if($this->player->distance($tile) < $this->player->distance($theChest)){
+                            $theChest = $tile;
+                        }
+                    }
+
+                    $chestCount++;
+                }
+            }
+        }
+
+        if($chestCount !== 0){
+            $replace = array(
+                "{chestCount}" => $chestCount,
+                "{chestDistance}" => round($this->player->distance($theChest), 0),
+                "{lineBreak}" => "\n"
+            );
+
+            $message = $this->plugin->config->get("chest-detected");
+
+            foreach($replace as $key => $value){
+                $message = str_replace($key, $value, $message);
+            }
+
+            $this->player->sendPopup($message);
+        } else {
+            $this->player->sendPopup($this->plugin->config->get("no-chest"));
+        }
+    }
+}
